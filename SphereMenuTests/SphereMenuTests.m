6b8337a67639c7db18fd7e1bebd78bf22bcea41a
tuyou <yoututouch@gmail.com>
2014-08-26 22:13:46 +0800

    codes submit

---
+++ SphereMenuTests/SphereMenuTests.m
@@ -0,0 +1,34 @@
+//
+//  SphereMenuTests.m
+//  SphereMenuTests
+//
+//  Created by Tu You on 14-8-24.
+//  Copyright (c) 2014年 TU YOU. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SphereMenuTests : XCTestCase
+
+@end
+
+@implementation SphereMenuTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
